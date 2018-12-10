5047700bfafe6165d947df6db9b6038430e70785
fabioyk <kyongxd@gmail.com>
2018-12-10 16:23:14 -0200

    fixed dungeon items on view being spoiled by search

--- src/app/game/item-log/log-search.pipe.ts
+++ src/app/game/item-log/log-search.pipe.ts
@@ -16,9 +16,9 @@ export class LogSearchPipe implements PipeTransform {
       return items;
     }
     const importantActions = ['blind', 'tt-bomb', 'switch', 'flood', 'ip-switch-room', 'hintTile'];
-
+console.log(items);
     return items.filter(item => {
-      if (importantActions.indexOf(item.shortName) > -1 && filterObj.searchQuery === '') {
+      if (importantActions.includes(item.shortName) && filterObj.searchQuery === '') {
         return true;
       }
       if (filterObj.onlyImportantShown && item.longName.indexOf('Agahnim') === -1 && item.longName.indexOf('Ganon') === -1 && item.item !== 'hint' && !ItemNamesService.isTrackableItem(+item.item) && (item.type !== 'view' && item.type !== 'cant')) {
@@ -31,12 +31,16 @@ export class LogSearchPipe implements PipeTransform {
         if (item.region.toLowerCase().indexOf(filterObj.searchQuery.toLowerCase()) > -1) {
           return true;
         }
-        if (this._itemNamesService.getItemById(item.item).longName.toLowerCase().indexOf(filterObj.searchQuery.toLowerCase()) > -1) {
-          return true;
-        }
-        if (item.item.toLowerCase().indexOf(filterObj.searchQuery.toLowerCase()) > -1) {
-          return true;
-        }
+        if (item.type !== "view" || (!item.shortName.includes('smallKey') && !item.shortName.includes('bigKey') && !item.shortName.includes('map') && !item.shortName.includes('compass'))) {
+          if (this._itemNamesService.getItemById(item.item).longName.toLowerCase().indexOf(filterObj.searchQuery.toLowerCase()) > -1) {
+            return true;
+          }
+          if (item.item.toLowerCase().indexOf(filterObj.searchQuery.toLowerCase()) > -1) {
+            return true;
+          }
+        } else {
+          return false;
+        }        
         return false;
       } else {
         return true;
